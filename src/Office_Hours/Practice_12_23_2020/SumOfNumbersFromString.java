537e24592dd9ac11f108e62e9f8bdf0cab9be146
yonashaile12 <yonashaile12@gmail.com>
2020-12-30 18:00:54 -0600

    Practice with Saim

--- src/Office_Hours/Practice_12_23_2020/SumOfNumbersFromString.java
+++ src/Office_Hours/Practice_12_23_2020/SumOfNumbersFromString.java
@@ -1,16 +1,22 @@
 package Office_Hours.Practice_12_23_2020;
 
+import java.util.Arrays;
+
 public class SumOfNumbersFromString {
 
     public static void main(String[] args) {
-        System.out.println(getSumOfNumbers("jav45ai15sgre100at82"));
-        System.out.println(getSumOfNumbers("abc12sd400sa9dhd83"));
+//        System.out.println(getSumOfNumbers("jav45ai15sgre100at82"));
+//        System.out.println(getSumOfNumbers("abc12sd400sa9dhd83"));
+
+        System.out.println(twoSum(new int[]{2,7,11,15},9));
     }
 
     public static int getSumOfNumbers(String str){
         // jav45ai15sgre100at82
         int sum = 0;
         String num = "";
+
+
 
         for(int i = 0; i< str.length(); i++){
             if(Character.isDigit(str.charAt(i))){
@@ -23,4 +29,56 @@ public class SumOfNumbersFromString {
         }
         return sum;
     }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        /*
+    Given an array of integers nums and an integer target,
+    return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution,
+    and you may not use the same element twice.
+    You can return the answer in any order.
+    Example 1:
+        Input: nums = [2,7,11,15], target = 9
+            Output: [0,1]
+            Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+         */
+        for(int i = 0; i< nums.length-1; i++){
+            for (int j = i + 1; j < nums.length; j++) {
+                if(nums[i] + nums[i] == target){
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {};
+    }
+
+    public int removeDuplicates(int[] nums) {
+        //Given a sorted array nums, remove the duplicates
+        // in-place such that each element appears only once and returns the new length.
+        //Do not allocate extra space for another array, you must do
+        // this by modifying the input array in-place with O(1) extra memory.
+        int length = nums.length;
+
+        if(length == 0)
+            return 0;
+
+
+        int targetIndex = 1;
+        int OG = nums[0];
+
+        for(int i = 1; i < length; i++){
+
+            if(nums[i] != OG){
+
+                OG = nums[i];
+                nums[targetIndex] = nums[i];
+                targetIndex++;
+            }
+        }
+
+        return targetIndex;
+    }
+
+    
 }
---
+++ src/Office_Hours/Practice_12_30_2020/BalanceBrackets.java
@@ -0,0 +1,48 @@
+package Office_Hours.Practice_12_30_2020;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class BalanceBrackets {
+
+    // {([])}
+
+    // ( { } )]
+
+    public static void main(String[] args) {
+        System.out.println(isBalanced("({})"));
+    }
+    public static boolean isBalanced(String str){
+
+        String open = "{[(";
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put('[',']');
+        map.put('{','}');
+        map.put('(',')');
+        for(int i = 0; i < str.length(); i++){
+
+            if(open.contains(""+str.charAt(i))){
+                // this char is some open bracket
+
+                stack.push(str.charAt(i));// {
+            }else {
+                // this char is some closing bracket
+
+                if (stack.isEmpty() || str.charAt(i) != map.get(stack.pop()) ){ // ] != ]
+                    return false;                                               // ) != )
+                }
+
+            }
+            /*
+                stack:
+                        [
+                        (
+                        {
+             */
+        }
+        return stack.isEmpty();
+
+    }
+}
